627b12f85706c7e390aedf1f60b4625948409603
blankKSH <K.Hein2389@student.leedsbeckett.ac.uk>
2024-10-02 20:04:48 +0100

    2

---
+++ Hello Kaung.py
@@ -0,0 +1,5 @@
+print("Hello, Kaung!")
+
+Yorkshire_TempC=38.4
+Yorkshire_TempF=(Yorkshire_TempC*9 /5)+32
+print("Over the summer, temperatures in Yorkshire reached", Yorkshire_TempC, "degree C.""\nThat is", round(Yorkshire_TempF, 2), "in  Fahrenheit")
